1e801036e43107898b5400c68f2550726196ff5e
SohilSheikh <SheikhSohil7021@gmail.com>
2024-08-13 09:57:45 +0530

    Binary Search Tree or BST

---
+++ JAVA-Assignment/DSA - Assignment/Revision_Section/src/BST/main.java
@@ -0,0 +1,23 @@
+package BST;
+
+public class main {
+    public static void main(String[] args) {
+        Binary_Search_Tree myBST = new Binary_Search_Tree();
+        myBST.insert(47);
+        myBST.insert(21);
+        myBST.insert(76);
+        myBST.insert(18);
+        myBST.insert(52);
+        myBST.insert(82);
+
+        myBST.insert(27);
+
+        System.out.println( myBST.contains(18));
+
+//        System.out.println("root = " + myBST.root);
+//        System.out.println(myBST.root.left.value);
+//        System.out.println(myBST.root.right.value);
+//        System.out.println(myBST.root.left.right.value);
+
+    }
+}
